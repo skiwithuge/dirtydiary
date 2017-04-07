5d1fc2cac8fcd9d614055bf6ccb2b3665c8b477a
blackgin <blackgins@gmail.com>
2017-04-07 19:58:06 +0200

    Day is immutable

--- app/src/main/java/com/skiwithuge/dirtydiary/model/Day.java
+++ app/src/main/java/com/skiwithuge/dirtydiary/model/Day.java
@@ -1,55 +1,30 @@
 package com.skiwithuge.dirtydiary.model;
 
-import java.util.Date;
-import java.util.UUID;
-
 /**
  * Created by skiwithuge on 4/3/17.
  */
 
 public class Day {
 
-    public UUID mId;
-    public String mDate;
-    public String mTitle;
-    public String mContent;
+    private String mDate;
+    private String mTitle;
+    private String mContent;
 
-    public Day(String date, String title, String content){
+    public Day(String date, String title, String content) {
         mDate = date;
         mTitle = title;
         mContent = content;
-    }
-
-    public UUID getId() {
-        return mId;
-    }
-
-    public void setId(UUID id) {
-        mId = id;
     }
 
     public String getDate() {
         return mDate;
     }
 
-    public void setDate(String date) {
-        mDate = date;
-    }
-
     public String getTitle() {
         return mTitle;
-    }
-
-    public void setTitle(String title) {
-        mTitle = title;
     }
 
     public String getContent() {
         return mContent;
     }
-
-    public void setContent(String content) {
-        mContent = content;
-    }
-
 }
