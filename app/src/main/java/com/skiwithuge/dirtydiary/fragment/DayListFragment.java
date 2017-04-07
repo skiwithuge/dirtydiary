cef7fff14f83ea7ff5a323a244a37ac49b87486a
blackgin <blackgins@gmail.com>
2017-04-07 20:16:56 +0200

    Adapter deserves its file :) + changed how clicks are handled

--- app/src/main/java/com/skiwithuge/dirtydiary/fragment/DayListFragment.java
+++ app/src/main/java/com/skiwithuge/dirtydiary/fragment/DayListFragment.java
@@ -2,17 +2,19 @@ package com.skiwithuge.dirtydiary.fragment;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.skiwithuge.dirtydiary.R;
+import com.skiwithuge.dirtydiary.adapter.DayAdapter;
 import com.skiwithuge.dirtydiary.interfaces.OnAddDayClickListener;
 import com.skiwithuge.dirtydiary.interfaces.OnDayClickListener;
+import com.skiwithuge.dirtydiary.interfaces.OnItemClickListener;
 import com.skiwithuge.dirtydiary.model.Day;
 import com.skiwithuge.dirtydiary.model.DayList;
 
@@ -26,8 +28,11 @@ import butterknife.OnClick;
  * Created by skiwithuge on 4/3/17.
  */
 
-public class DayListFragment extends Fragment {
-    @BindView(R.id.day_recycler_view) RecyclerView mDayRecyclerView;
+public class DayListFragment extends Fragment implements OnItemClickListener<Day> {
+
+    @BindView(R.id.day_recycler_view)
+    RecyclerView mDayRecyclerView;
+
     private DayAdapter mAdapter;
     private OnDayClickListener mOnDayClickListener;
     private OnAddDayClickListener mOnAddDayClickListener;
@@ -51,12 +56,15 @@ public class DayListFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_day_list, container, false);
         ButterKnife.bind(this, view);
 
-        //mDayRecyclerView = (RecyclerView) view
-        //        .findViewById(R.id.day_recycler_view);
-        mDayRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-
-        updateUI();
         return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mDayRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        updateUI();
     }
 
     @Override
@@ -70,7 +78,7 @@ public class DayListFragment extends Fragment {
         List<Day> days = dl.getDays();
 
         if (mAdapter == null) {
-            mAdapter = new DayAdapter(getActivity(), days);
+            mAdapter = new DayAdapter(days, this);
             mDayRecyclerView.setAdapter(mAdapter);
         } else {
             mDayRecyclerView.setAdapter(mAdapter);
@@ -98,73 +106,14 @@ public class DayListFragment extends Fragment {
     //TODO onOptionsItemSelected
 
     @OnClick(R.id.fab_add)
-    public void addClick(){
+    public void addClick() {
         mOnAddDayClickListener.onAddDayClick();
     }
 
-    public class DayHolder extends RecyclerView.ViewHolder
-            implements View.OnClickListener {
-        @BindView(R.id.card_date) TextView mDate;
-        @BindView(R.id.card_title) TextView mTitle;
 
-        private Day mDay;
-
-        public DayHolder(View itemView) {
-            super(itemView);
-            itemView.setOnClickListener(this);
-            ButterKnife.bind(this, itemView);
-
-
-        }
-
-        public void bindDay(Day day) {
-            mDay = day;
-
-            mDate.setText(mDay.getDate());
-            mTitle.setText(mDay.getTitle());
-
-        }
-
-        //TODO CHECK
-        @Override
-        public void onClick(View view) {
-            mOnDayClickListener.onDaySelected(mDay);
-        }
+    @Override
+    public void onItemClick(Day item) {
+        mOnDayClickListener.onDaySelected(item);
     }
 
-    private class DayAdapter extends RecyclerView.Adapter<DayHolder> {
-        private List<Day> mDays;
-        private Context mContext;
-
-        public DayAdapter(List<Day> days){
-            mDays = days;
-        }
-
-        public DayAdapter(Context context, List<Day> days){
-            mDays = days;
-            mContext = context;
-        }
-
-        @Override
-        public DayHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            LayoutInflater layoutInflater = LayoutInflater.from(mContext);
-            View view = layoutInflater.inflate(R.layout.list_item_day, parent, false);
-            return new DayHolder(view);
-        }
-
-        @Override
-        public void onBindViewHolder(DayHolder holder, int position) {
-            Day day = mDays.get(position);
-            holder.bindDay(day);
-        }
-
-        @Override
-        public int getItemCount() {
-            return mDays.size();
-        }
-
-        public void setDays(List<Day> days) {
-            mDays = days;
-        }
-    }
 }
---
+++ app/src/main/java/com/skiwithuge/dirtydiary/interfaces/OnItemClickListener.java
@@ -0,0 +1,9 @@
+package com.skiwithuge.dirtydiary.interfaces;
+
+/**
+ * Created by gianmarcodavid on 07/04/17.
+ */
+
+public interface OnItemClickListener<T> {
+    void onItemClick(T item);
+}
